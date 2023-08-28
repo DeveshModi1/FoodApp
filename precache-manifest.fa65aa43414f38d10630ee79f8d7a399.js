0f4ea6e520bc9586d6520031c741266e03af3e8f
DeveshModi1 <modidevesh15@gmail.com>
2023-08-28 23:19:53 +0530

    Updates

---
+++ precache-manifest.fa65aa43414f38d10630ee79f8d7a399.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef27f3412159eeb3e6d5",
+    "url": "/FoodApp/static/css/main.7f50fb85.chunk.css"
+  },
+  {
+    "revision": "ef27f3412159eeb3e6d5",
+    "url": "/FoodApp/static/js/main.ef27f341.chunk.js"
+  },
+  {
+    "revision": "5df50f20135844d1f62b",
+    "url": "/FoodApp/static/js/1.5df50f20.chunk.js"
+  },
+  {
+    "revision": "11f11fe1db3c8fa54b8a",
+    "url": "/FoodApp/static/css/2.b1deeb00.chunk.css"
+  },
+  {
+    "revision": "11f11fe1db3c8fa54b8a",
+    "url": "/FoodApp/static/js/2.11f11fe1.chunk.js"
+  },
+  {
+    "revision": "e3ae5ec1b50075e1728b",
+    "url": "/FoodApp/static/js/runtime~main.e3ae5ec1.js"
+  },
+  {
+    "revision": "cde03377577a476c693c69295794bc3b",
+    "url": "/FoodApp/static/media/remove.cde03377.png"
+  },
+  {
+    "revision": "5fcde1506dfa359f719b6725e3cc65fb",
+    "url": "/FoodApp/index.html"
+  }
+];
